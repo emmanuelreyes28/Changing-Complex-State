1cf9741c8e12e234e1644105bdce300f8ba5ff1d
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-11-04 16:25:44 -0700

    Changing Complex State

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,54 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  //useState here uses an object that hold both first and last name
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
 
-  function changeFirstName(event) {
-    setFirstName(event.target.value);
-  }
+  function handleChange(event) {
+    // const newValue = event.target.value; //grabs value entered in input
+    // const inputName = event.target.name; //grabs element name from input tag
 
-  function changeLastName(event) {
-    setLastName(event.target.value);
+    //use object destructuiring to make code above more simple
+    const { value, name } = event.target;
+
+    //prevValue keeps the previous value of a key when another is being changed
+    //so it does not get removed. Note: DO NOT USE EVENT INSIDE OF SET STATE
+    setFullName((prevValue) => {
+      //check what input was changed and update values accordingly
+      if (name === "fName") {
+        return {
+          fName: value, //update with new value
+          lName: prevValue.lName, //reuse prev value
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+        };
+      }
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {firstName} {lastName}
+        Hello {fullName.fName} {fullName.lName}
       </h1>
       <form>
         <input
-          onChange={changeFirstName}
+          onChange={handleChange}
           name="fName"
           placeholder="First Name"
-          value={firstName}
+          value={fullName.fName} //when we assign the value with useState it becomes controlled
         />
         <input
-          onChange={changeLastName}
+          onChange={handleChange}
           name="lName"
           placeholder="Last Name"
-          value={lastName}
+          value={fullName.lName}
         />
         <button>Submit</button>
       </form>
